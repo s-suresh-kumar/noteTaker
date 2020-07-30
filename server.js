a04e24b59193c022e11b29977a3abce1771c3f3e
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-07-30 08:39:09 -0400

    next version

--- server.js
+++ server.js
@@ -1,83 +1,62 @@
-// Dependencies
-// =============================================================
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
-const writeFileAsync = util.promisify(fs.writeFile);
-const readFileAsync = util.promisify(fs.readFile);
-var express = require("express");
-var path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+const mainDir = path.join(__dirname, "/public");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
-
-// Sets up the Express app to handle data parsing
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-
-// List of Notes (DATA)
-// =============================================================
-var notes = [
-  {
-    title: "Test Title",
-    text: "Test text",
-  },
-];
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(mainDir, "notes.html"));
 });
 
-// Displays all characters
 app.get("/api/notes", function (req, res) {
-  return res.json(characters);
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-// // Displays a single character, or returns false
-// app.get("/api/characters/:character", function(req, res) {
-//     var chosen = req.params.character;
+app.get("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
 
-//     console.log(chosen);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
 
-//     for (var i = 0; i < characters.length; i++) {
-//         if (chosen === characters[i].routeName) {
-//             return res.json(characters[i]);
-//         }
-//     }
-
-//     return res.json(false);
-// });
-
-// Create New note - takes in JSON input
 app.post("/api/notes", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let uniqueID = savedNotes.length.toString();
+  newNote.id = uniqueID;
+  savedNotes.push(newNote);
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  console.log("Note saved to db.json. Content: ", newNote);
+  res.json(savedNotes);
 });
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+app.delete("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  console.log(`Deleting note with ID ${noteID}`);
+  savedNotes = savedNotes.filter((currNote) => {
+    return currNote.id != noteID;
+  });
+
+  for (currNote of savedNotes) {
+    currNote.id = newID.toString();
+    newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+});
+
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}. At your service!`);
 });
